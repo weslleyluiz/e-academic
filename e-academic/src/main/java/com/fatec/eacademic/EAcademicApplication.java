c829f5e8658de0bd2406cb5b43010f340f6b8c10
Weslley Luiz <weslleyluiz@outlook.com.br>
2020-11-16 22:27:14 -0300

    Initial commit

---
+++ e-academic/src/main/java/com/fatec/eacademic/EAcademicApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.eacademic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EAcademicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EAcademicApplication.class, args);
+	}
+
+}
